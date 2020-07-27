9ccc8cd3e1ef5aa6fd49ea6d20da37e783178ac1
JY20 <43075399+JY20@users.noreply.github.com>
2020-07-27 14:37:55 -0400

    learn about Hook
    try out hook and try to make parts visible.

--- src/App.js
+++ src/App.js
@@ -1,62 +1,21 @@
-import React from "react";
 import logo from "./logo.svg";
+import React, { useState } from "react";
 import "./App.css";
-// import Header from "./components/header";
-// import Body, { Body2 } from "./components/body";
-// import Count from "./components/counter";
 import ImageSlider from "./components/imageslider";
 
 function App() {
-  //const add = (a, b) => a + b;
+  const [visible, setVisible] = useState(false);
 
-  state = {
-    visible: true,
+  const ButtonH = () => {
+    console.log("hii");
+    setVisible(false);
   };
+
   return (
-    // <div className="App">
-    //   {
-    //     <header className="App-header">
-    //       <img src={logo} className="App-logo" alt="logo" />
-    //       <Header
-    //         hhh
-    //         til="hshsh"
-    //         ti={"sss"}
-    //         num={11}
-    //         myarr={[1, 2, 23]}
-    //         myfun={add}
-    //         ofun={(a, b) => a + b}
-    //         myobj={{
-    //           a: 5,
-    //           b: 2,
-    //         }}
-    //       />
-    //       <br></br>
-    //       <Count />
-    //       <br></br>
-    //       <ImageSlider />
-    //       <a
-    //         className="App-link"
-    //         href="https://drive.google.com/drive/u/0/my-drive"
-    //         target="_blank"
-    //         rel="noopener noreferrer"
-    //       >
-    //         Learn React
-    //         <Body />
-    //       </a>
-    //     </header>
-    //   }
-    // </div>
     <div className="App">
-      {this.state.visible ? <ImageSlider /> : null}
-      <button
-        onClick={() => {
-          this.setState({
-            visible: false,
-          });
-        }}
-      >
-        Hide
-      </button>
+      <div> huii</div>
+      <button onClick={ButtonH}>HIIIII</button>
+      {visible ? <ImageSlider /> : <div>JJJJJJJJJJ</div>}
     </div>
   );
 }
